ed30b393dc5291b08ac0cced903057d1038fc40f
erinmilling <mill0814@algonquinlive.com>
2016-03-17 19:29:11 -0400

    added above elements

--- js/main.js
+++ js/main.js
@@ -3,9 +3,13 @@ var $ulBelow = $('<ul>');
 var $h1Above = $('<h1>');
 var $ulAbove = $('<ul>');
 
-$h1Below.html($h1Below);
+$h1Below.html('Below Ground Veggies');
 
 $('body').append($h1Below);
 $('body').append($ulBelow);
 
+$h1Above.html('Above Ground Veggies');
+
+$('body').append($h1Above);
+$('body').append($ulAbove);
 });
