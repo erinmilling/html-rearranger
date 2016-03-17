225e1fd0c0ca271c19999dd04af3a293acb4757d
erinmilling <mill0814@algonquinlive.com>
2016-03-17 19:25:45 -0400

    added below elements

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@ var $h1Below = $('<h1>');
 var $ulBelow = $('<ul>');
 var $h1Above = $('<h1>');
 var $ulAbove = $('<ul>');
+
+$h1Below.html($h1Below);
+
+$('body').append($h1Below);
+$('body').append($ulBelow);
+
+});
